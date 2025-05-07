b675e66880e3d5e45ccc9de5d98924e48deb1f9b
Chri-Libra-Unipd <chistian.libralato@studenti.unipd.it>
2025-05-07 22:17:04 +0200

    scrittura test per IntegerToRoman e RomanPrinter close #12 close #13

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,9 +1,10 @@
-////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////// 
  // [Davide] [Lorenzon] [2 101 075] 
  // [Christian] [Libralato] [ 2 101 047] 
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;  
   
+ import static org.junit.Assert.assertEquals;
  import static org.junit.Assert.assertTrue;
  import org.junit.Test; 
   
@@ -20,6 +21,18 @@ package it.unipd.mtss;
      { 
          assertTrue( true ); 
      } 
+     
+     @Test 
+     public void test_56(){
+        String expected=
+        " _       __      __  _____  \n"+
+        "| |      \\ \\    / / |_   _| \n"+
+        "| |       \\ \\  / /    | |   \n"+
+        "| |        \\ \\/ /     | |   \n"+
+        "| |____     \\  /     _| |_  \n"+
+        "|______|     \\/     |_____| \n";
+        assertEquals(expected,RomanPrinter.print(56));
+    }
  } 
   
  
