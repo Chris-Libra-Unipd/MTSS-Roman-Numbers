345fb8db97a2186fecdcd801b6ac51bc40235ef5
D-lore-unipd <root@LaptopDavide.>
2025-05-07 21:56:12 +0200

    inizializzati file sorgenti e test, close #8

---
+++ src/test/java/it/unipd/mtss/RomanLettersTest.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+ // [Davide] [Lorenzon] [2 101 075] 
+ // [Christian] [Libralato] [ 2 101 047] 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;  
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class RomanLettersTest {
+
+ @Test 
+ public void shouldAnswerWithTrue() { 
+     assertTrue( true ); 
+ } 
+
+
+}
