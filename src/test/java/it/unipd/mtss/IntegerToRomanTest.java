0cde6d7bfc846755a963811bc345e8af3cfc81db
Chri-Libra-Unipd <chistian.libralato@studenti.unipd.it>
2025-05-07 22:22:19 +0200

    scrittura test per IntegerToRoman e RomanPrinter close #12 close #13

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -30,7 +30,7 @@ import org.junit.Test;
         int[] numbers = {1,2,3};
         String[] romanN = {"I","II","III"};
         for(int i = 0; i < 3; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
 
      @Test
@@ -38,7 +38,7 @@ import org.junit.Test;
         int[] numbers = {4,5,6,7,8};
         String[] romanN = {"IV","V","VI","VII","VIII"};
         for(int i = 0; i < 5; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
     
      @Test
@@ -46,7 +46,7 @@ import org.junit.Test;
         int[] numbers = {9,10,11,12,13,14,15,16,17,18,19,20};
         String[] romanN = {"IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"};
         for(int i = 0; i < 12; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
 
      @Test
@@ -54,7 +54,7 @@ import org.junit.Test;
         int[] numbers = {21,25,29,30,35,37,41,44,46,49};
         String[] romanN = {"XXI","XXV","XXIX","XXX","XXXV","XXXVII","XLI","XLIV","XLVI","XLIX"};
         for(int i = 0; i < 10; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
 
      @Test
@@ -62,7 +62,7 @@ import org.junit.Test;
         int[] numbers = {50,57,60,63,69,72,78,84,90,99};
         String[] romanN = {"L","LVII","LX","LXIII","LXIX","LXXII","LXXVIII","LXXXIV","XC","XCIX"};
         for(int i = 0; i < 10; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
 
 
@@ -71,7 +71,7 @@ import org.junit.Test;
         int[] numbers = {100,107,158,212,269,340,398,432,471,499};
         String[] romanN = {"C","CVII","CLVIII","CCXII","CCLXIX","CCCXL","CCCXCVIII","CDXXXII","CDLXXI","CDXCIX"};
         for(int i = 0; i < 10; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
 
      @Test
@@ -79,16 +79,14 @@ import org.junit.Test;
         int[] numbers = {500,539,596,643,677,711,767,843,945,999};
         String[] romanN = {"D","DXXXIX","DXCVI","DCXLIII","DCLXXVII","DCCXI","DCCLXVII","DCCCXLIII","CMXLV","CMXCIX"};
         for(int i = 0; i < 10; i++)
-            assertEquals(romanN[i],ITR.convert(numbers[i]));
+            assertEquals(romanN[i],IntegerToRoman.convert(numbers[i]));
      }
 
      @Test
      public void test1000(){
-        assertEquals("M",ITR.convert(1000));
+        assertEquals("M",IntegerToRoman.convert(1000));
      }
 
-
-         assertTrue( true); 
      }
 
 
