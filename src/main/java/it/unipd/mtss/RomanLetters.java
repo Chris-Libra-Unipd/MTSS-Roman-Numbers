a37ff279c6c877a26c02670839b229010db34f8d
D-lore-unipd <root@LaptopDavide.>
2025-05-07 22:30:17 +0200

    implementazione dei test di RomanLettersTest e implementazione minimale di RomanLetters, a fini di compilazione (i test falliscono) , close #14

--- src/main/java/it/unipd/mtss/RomanLetters.java
+++ src/main/java/it/unipd/mtss/RomanLetters.java
@@ -7,6 +7,25 @@ package it.unipd.mtss;
 //classe di supporto per la stampa in ascii art
 
 public enum RomanLetters {
+    I(null),
+    V(null),
+    X(null),
+    L(null),
+    C(null),
+    D(null),
+    M(null);
 
+    private final String[] asciiArt;
 
+    private RomanLetters(String[] asciiArt) {
+        this.asciiArt = null;
+    }
+
+    public String[] getAsciiArt() {
+        return null;
+    }
+
+    public static RomanLetters fromChar(char c) {
+        throw new UnsupportedOperationException("Non ancora implementato");
+    }
 }
